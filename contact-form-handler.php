c302cb2aabe20613e1bfd35abe1ac4be2412d98e
Braedon Merlo <bdmerlo@gmail.com>
2018-04-23 09:31:22 -0700

    first commit

---
+++ contact-form-handler.php
@@ -0,0 +1,28 @@
+<?php
+    $name = $_POST['name'];
+    $visitor_email = $_POST['email'];
+    $number = $_POST['number'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+
+    $email_from = 'threenpainting@yahoo.com';
+
+    $email_subject = "$subject";
+
+    $email_body = "User Name: $name.\n".
+                    "User Email: $visitor_email.\n".
+                        "User Number: $number.\n".
+                            "User Subject: $subject.\n".
+                                "User Message: $message.\n";
+
+
+    $to = "threenpainting@yahoo.com";
+
+    $headers = "From: $email_from \r\n";
+    $headers = "Reply-to: $visitor_email \r\n";
+
+    mail($to, $email_subject, $email_body, $headers);
+
+    header("Location: home.html");
+
+?>
